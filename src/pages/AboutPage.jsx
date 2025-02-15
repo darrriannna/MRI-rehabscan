9fc97859eda3bee867b7d6167ade7a991664cb4f
darynafesyk <darynafesyk@gmail.com>
2025-02-15 10:31:58 +0100

    fixes 2 files

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -18,7 +18,7 @@ const AboutPage = () => {
       <img src="./assets/About-img.png" alt="RehabScan Sverige - Magnetröntgen utan remiss" />
     </div>
     <div className="right-column">
-    <p className='text-about p-4'>RehabScan är en digital hälsoklinik som erbjuder radiologiska undersökningar som magnetresonans, CT röntgen och konventionell röntgen över hela Sverige.</p>
+    <p className='text-about p-4'>RehabScan erbjuder MR-röntgenundersökningar i hela landet utan krav på remiss för att ge dig en bättre inblick i din hälsa.</p>
     <p className='p-4'>Med RehabScan kan du känna dig trygg i att få den vård och uppmärksamhet du behöver – Din hälsa prioriteras hos oss och vi strävar efter att ge dig den bästa möjliga upplevelsen, oavsett ditt behov.
     Välkommen till RehabScan, en hälsoklinik i framkant som sätter dig och din hälsa i fokus!</p>
     </div>
