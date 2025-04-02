fbf51ff674fd126379df2f7bda8c867f24575e7f
darynafesyk <darynafesyk@gmail.com>
2025-04-02 10:46:51 +0200

    updates

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,19 +5,30 @@ const Partners = () => {
   const partners = [
     { image: '/assets/evidia.png' },
     { image: '/assets/unilabs.png' },
-    { image: '/assets/aleris.png' },
+
+  ];
+  const cards = [
     { image: '/assets/klarna.png' },
     { image: '/assets/visa.png' },
     { image: '/assets/mastercard.png' },
-  ];
+  ]
 
   return (
     <div className="partners-container">
-      <h3 className="text-center header p-3">Våra samarbetspartners</h3>
+      <h3 className="text-center header p-3">Undersökningen görs av</h3>
       <div className="partner-grid">
       {partners.map((partner, index) => (
           <div key={index} className="partner-card">
             <img src={partner.image} alt={`Partner ${index + 1}`} className="partner-image" />
+          </div>
+        ))} 
+       
+      </div>
+      <h3 className="text-center header p-3">Betala med</h3>
+      <div className="partner-grid">
+      {cards.map((card, index) => (
+          <div key={index} className="partner-card">
+            <img src={card.image} alt={`Card ${index + 1}`} className="partner-image" />
           </div>
         ))} 
        
