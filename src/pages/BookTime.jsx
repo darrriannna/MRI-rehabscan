1eae923635c1df1766cbae81b36ed9957271a808
darynafesyk <darynafesyk@gmail.com>
2025-05-07 18:35:54 +0200

    updates

--- src/pages/BookTime.jsx
+++ src/pages/BookTime.jsx
@@ -12,7 +12,12 @@ const BookTime = () => {
       <div className='message-after'>
       <div className='grid-deceases'>
           <div className='containers-deceases-info' >
-          <h4 className='text-center-info'>Efter att du har bokat skickar vi din remiss till den närmaste kliniken eller den klinik du har valt. Kliniken kontaktar dig inom ett par dagar för att hitta en tid som passar dig bäst. </h4>
+          <h4 className='text-center-info'>Efter att du har bokat skickar vi din remiss till den närmaste kliniken eller den klinik du har valt. Kliniken kontaktar dig inom ett par dagar för att hitta en tid som passar dig bäst.</h4>
+        </div></div></div>
+        <div className='message-after'>
+      <div className='grid-deceases'>
+          <div className='containers-deceases-info' >
+          <h4 className='text-center-info'> Uteblivet besök eller avbokning senare än 24 timmar innan inbokad tid debiteras fullt och återbetalas ej.  </h4>
         </div></div></div>
       <StepsIcons/>
       <Footer/>
